ee040cd942e3015cbf88348927e59e94a279db17
Ivan Chutkan <alexandrbozhkov1977@gmail.com>
2020-02-12 11:36:31 +0200

    Change mistakes

--- script.js
+++ script.js
@@ -6,43 +6,51 @@ const todoCont = document.getElementById('todo');
 const completedCont = document.getElementById('completed');
 const headerInput = document.querySelector('.header-input');
 
+// создаем объект
+
 let data = {
 
     todo: [],
     completed: []
 };
 
+// проверка LocalStorage на наличие данных
+
 if(localStorage.getItem('localData')) {
-    data.JSON.parse(localStorage.getItem('localData'));
+    data = JSON.parse(localStorage.getItem('localData'));
 }
 
+// рендер сохраненных (если есть) данных в LocalStorage
+
 const renderUpdateItems = function() {
-if(!data.todo && !data.complete) {
+if(!data.todo && !data.completed) { 
 return;
-} else {
+}
 for(let i = 0; i < data.todo.length; i++) {
 renderItem(data.todo[i]);
 }
-for(let i = 0; i < data.complete.length; i++) {
-renderItem(data.complete[i], true);
-}
+for(let i = 0; i < data.completed.length; i++) {
+renderItem(data.completed[i], true);
 }
 };
+
+// функция которая заносит данные в Storage
 
 const dataUpdateStorage = function() {
 localStorage.setItem('localData', JSON.stringify(data));
 };
 
-form.addEventListener('submit', function(event) {
-event.preventDefault();
+// функция добавляет элемент на страницу 
 
 const addItem = function(text) {
 renderItem(text);
-headerInput.value = '';
-data.todo.push(text);
+headerInput.value = ''; // очистка поля ввода
+data.todo.push(text); // отправка данных
 
-dataUpdateStorage();
+dataUpdateStorage(); // обновление Storage
 };
+
+// Функция удаляет элемент из родителя (todo)
 
 const itemRemove = function(elem) {
 const item = elem.parentNode.parentNode;
@@ -60,6 +68,8 @@ itemParent.removeChild(item);
 
 dataUpdateStorage();
 };
+
+// переносит элемент
 
 const itemComplete = function(elem) {
 const item = elem.parentNode.parentNode;
@@ -88,6 +98,8 @@ target.insertBefore(item, target.childNodes[0]);
 dataUpdateStorage();
 };
 
+// Функция рендеринга одного элемента
+
 const renderItem = function(text, completed = false) {
 
 // создаем элементы    
@@ -109,8 +121,6 @@ btnBlock.classList.add('todo-buttons');
 btnRemove.classList.add('todo-remove');
 btnComplete.classList.add('todo-complete');
 
-item.textContent = text;
-
 btnRemove.addEventListener('click', function(event) {
 itemRemove(event.target);
 });
@@ -119,6 +129,7 @@ btnComplete.addEventListener('click', function(event) {
 itemComplete(event.target);
 });
 
+item.textContent = text;
 //создаем блоки
 btnBlock.appendChild(btnRemove);
 btnBlock.appendChild(btnComplete);
@@ -126,10 +137,15 @@ item.appendChild(btnBlock);
 
 // помещаем созданный блок item в блок todoCont перед первым(индекс[0]) "ребенком" блока 
 list.insertBefore(item, list.childNodes[0]);
+
 };
 
+// обработчик формы
+form.addEventListener('submit', function(event) {
+event.preventDefault();
+
 if(headerInput.value !== '') {
-addItem(headerInput.value);
+addItem(headerInput.value.trim());
 }
 });
 
